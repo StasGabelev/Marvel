d73b2653aeff6191b43d08db407af0e446eb8a2f
Stanislav <stanislavgabelev@gmail.com>
2022-05-31 21:40:18 +0300

    new one

--- js/script.js
+++ js/script.js
@@ -21,26 +21,74 @@
 
 
 
-const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели", "");
+// const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели", "");
  
-const personalMovieDB = {
-    count: numberOfFilms, 
-    movies: {}, 
-    actors: {}, 
-    genres: [], 
-    privat: false
-    };
+// const personalMovieDB = {
+//     count: numberOfFilms, 
+//     movies: {}, 
+//     actors: {}, 
+//     genres: [], 
+//     privat: false
+//     };
 
                     
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его?", "");
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = prompt("На сколько оцените его?", "");
+// const a = prompt("Один из последних просмотренных фильмов?", "");
+// const b = prompt("На сколько оцените его?", "");
+// const c = prompt("Один из последних просмотренных фильмов?", "");
+// const d = prompt("На сколько оцените его?", "");
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+///////////////////////////////////////////////////////////////
+// let num = 50;
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <= 55);
+
+////////////////////////////////////////////////////////////////
+
+let num = 50;
+
+// for (let i=1; i < 8; i++) {
+//     console.log(i);
+// }
+////////////////////////////////////////////////////////////////
+// for (let i = 1; i <10; i++) {
+//     console.log(num);
+//     num++;
+// }
+////////////////////////////////////////////////////////////////
+// for (let i = 1; i < 10; i++) {
+//     if (i === 7) {
+//         break;
+//     }
+//     console.log(i);
+// }
+////////////////////////////////////////////////////////////////
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+//////////////////////////////////////////////////////////////
 
